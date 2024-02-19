bd26320904932cf023f5cfa6e79a29a5efda1754
Dominic  Charles <dominiccharles@Dominics-MacBook-Pro.local>
2024-02-18 19:33:14 -0800

    adding all files to git, initial

---
+++ rampOpen.js
@@ -0,0 +1,17 @@
+var total = 0;
+var single = 0;
+var taken = 0;
+
+function addSingle() {
+    total ++;
+    single ++;
+    var singlePercentage = single / total;
+    var takenPercentage = taken / total;
+    return singlePercentage, takenPercentage
+}
+
+function displayText() {
+    var text = single;
+    // var text = document.getElementById("takenPercentage-result");
+    text.style.display = "block";
+}
